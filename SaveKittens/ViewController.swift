39054937c6b19da66653656fcb7874fa498b0dfb
Vaheh Hatami <vahehadamian@gmail.com>
2017-02-01 12:24:59 +0100

    Just did it again
    trying out

--- SaveKittens/ViewController.swift
+++ SaveKittens/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Hi there, I changed")
+        print("Hi there, I changed")
     }
 
     override func didReceiveMemoryWarning() {
